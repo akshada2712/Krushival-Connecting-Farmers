a48acb14003d0d8b1157f49930501bdddb5e31ad
Akshada Bhandari <akshada3654@gmail.com>
2020-11-25 21:43:31 +0530

    Fragment added

---
+++ app/src/main/java/com/example/krushival/adapter/CategoryAdapter.java
@@ -0,0 +1,45 @@
+package com.example.krushival.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.krushival.domain.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private Context context;
+    private List<Category> mCategoryList;
+
+    public CategoryAdapter(Context context, List<Category> mCategoryList) {
+        this.context = context;
+        this.mCategoryList = mCategoryList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCategoryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/krushival/domain/Category.java
@@ -0,0 +1,25 @@
+package com.example.krushival.domain;
+
+public class Category {
+    String type;
+    String img_url;
+
+    public Category() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+}
